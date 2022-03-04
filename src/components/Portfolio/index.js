4c9690a4bb15204451be49ff3ed4de23df43cce3
spaghettimedic <dsteiner247@gmail.com>
2022-03-04 15:41:33 -0500

    added <h5> to bottom of Portfolio page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,6 +6,7 @@ function Portfolio() {
     <section>
       <h3>Explore my work.</h3>
       <Project />
+      <h5>Click on the Github icon below to see all of my repositories.</h5>
     </section>
   );
 };
