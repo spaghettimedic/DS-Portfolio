38756382e299ba7ffb89c78f0226beb480552ebd
spaghettimedic <dsteiner247@gmail.com>
2022-03-04 13:03:53 -0500

    finished about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import david from '../../assets/David.jpg';
 
 function About() {
 
   return (
     <section>
-      <h3 className="ps-3 pt-3" id="about">Who am I?</h3>
-      {/* <img /> will place a centered image here */}
-      <article className="ps-3 pt-3">Thank you for coming to my professional profile. My name is David and I'd love to show you what I can do.</article>
+      <div className="avatar-container">
+        <img className="avatar" src={david} alt="David with a puppy" />
+      </div>
+      <article className="ps-3 pt-3">&nbsp;&nbsp;&nbsp;&nbsp;Welcome to my professional portfolio. My name is David and I'm glad you're here.
+      <br/><br/>
+      &nbsp;&nbsp;&nbsp;&nbsp;I have spent most of my adulthood working in emergency medical services as a paramedic, but recently I decided to make a switch into web development. I attended a full stack coding bootcamp through Michigan State University, learning the MERN stack. The portfolio tab above has links to all the projects that I have created in the bootcamp, as well as some personal projects. Also available here are navigation links for a contact form and my resume.
+      <br/><br/>
+      &nbsp;&nbsp;&nbsp;&nbsp;Take a look around and feel free to contact me with any questions or requests.
+      </article>
     </section>
   );
 };
