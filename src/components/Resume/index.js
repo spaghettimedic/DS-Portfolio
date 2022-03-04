0895ab1ac0baed36c3fcbd3b08bcbe507dc282e6
spaghettimedic <dsteiner247@gmail.com>
2022-03-04 16:10:01 -0500

    made downloadable link to resume and a list of proficiencies

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,7 +3,24 @@ import React from 'react';
 function Resume() {
   return(
     <section>
-      <p>This will be the RESUME section</p>
+      <br/>
+      <h3><a href="../../assets/Resume.pdf" download>Download a copy of my resume here.</a></h3>
+      <br/>
+      <article>
+        <ul>
+          <h3>Proficiencies:</h3>
+          <br/>
+          <li>HTML & CSS: BootStrap, Handlebars</li>
+          <li>JavaScript & jQuery, Node</li>
+          <li>Git, GitHub, Heroku</li>
+          <li>APIs: web, third-party, and server-side</li>
+          <li>RESTful APIs, Express</li>
+          <li>OOP, ORM, TDD, MVC</li>
+          <li>SQL, Sequelize, Insomnia</li>
+          <li>NoSQL, GraphQl, MongoDB, Mongoose</li>
+          <li>Progressive Web Applications</li>
+        </ul>
+      </article>
     </section>
   );
 };
